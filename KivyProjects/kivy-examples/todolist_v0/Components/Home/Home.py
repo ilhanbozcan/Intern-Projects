09627c528487bf5562758a469d4d41e5a9620e19
Nejat Cakal <nejatcakal@gmail.com>
2019-07-16 18:35:50 +0300

    Sil Butonunu Screen için çalıoşır duruma getirdim

--- KivyProjects/kivy-examples/todolist_v0/Components/Home/Home.py
+++ KivyProjects/kivy-examples/todolist_v0/Components/Home/Home.py
@@ -1,3 +1,4 @@
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
@@ -12,48 +13,27 @@ from Components.SM import SM
 class Home(Screen):
     def __init__(self,**kwargs):
         super(Home,self).__init__(**kwargs)
-
         EachTask.RemoveWidget = self.removeWidget
 
-        """
-        for task in tasks:
-            self.ids.add_field(Home.EachTask(text=task))
-        """
 
-    def removeWidget(self,instance):
-        children = self.ids.add_field.children
-        self.ids.add_field.remove_widget(self.ids.add_field.ids[instance.parent.id])
-
-        print(self.ids.add_field.children)
-        #self.ids.add_field.clear_widgets()
-        #for child in children:
-            #self.ids.add_field.add_widget(child)
-
-        #print(instance.)
-        #self.remove_widget(instance.parent)
-        #self.clear_widgets()
-
+    def removeWidget(self, instance):
+        self.ids.add_field.remove_widget(instance.parent.parent)
 
     def addWidget(self):
         input = self.ids.input.text
-        newListItem = EachTask(text=input, id=str((len(self.ids.add_field.children))) )
-        print(newListItem.id)
+        newListItem = EachTask(text=input, id=str((len(self.ids.add_field.children))))
         self.ids.add_field.add_widget(newListItem)
-        print(self.ids.add_field.children)
-
-
-
-
-
 
 class EachTask(BoxLayout):
-    def __init__(self, text= "", **kwargs):
-        super(EachTask,self).__init__(**kwargs)
+
+    def __init__(self, text="", **kwargs):
+        super(EachTask, self).__init__(**kwargs)
         self.ids.label.text = text
 
 
     def RemoveWidget(self,instance):
         pass
+
 
 
 
